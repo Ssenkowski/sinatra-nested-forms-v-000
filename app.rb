65c51bf83aabca04ed5aef654795044dca7afb46
Steven Senkowski <scsenkowski@gmail.com>
2018-01-08 14:19:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,9 +16,10 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
-      end
+      @pirate
+      # params[:pirate][:ships].each do |details|
+      #   Ship.new(details)
+      # end
       # @ships = Ship.all
       #
       # erb :'pirates/show'
