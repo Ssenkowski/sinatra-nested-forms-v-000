9a9a327ae36f7ca63e376718e8dec6fc1f9441d4
Steven Senkowski <scsenkowski@gmail.com>
2018-01-08 13:39:21 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(args)
+    @name, @type, @height = args[:name], args[:type], args[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
 end
